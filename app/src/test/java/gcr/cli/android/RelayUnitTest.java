514f035ec0260d7a2ec3e61951109912a708b433
Near Lg <nearlg@gmail.com>
2018-03-24 17:44:53 +0100

    Fix: This is a check-point to merge from next

--- app/src/test/java/gcr/cli/android/RelayUnitTest.java
+++ app/src/test/java/gcr/cli/android/RelayUnitTest.java
@@ -2,14 +2,17 @@ package gcr.cli.android;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 
 import gcr.cli.android.models.IRelay;
 import gcr.cli.android.models.Relay;
 import gcr.cli.android.validatiors.IModelValidator;
-import gcr.cli.android.validatiors.RelayModelValidator;
+import gcr.cli.android.validatiors.ModelValidator;
+import gcr.cli.android.validatiors.RelayValidator;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -19,11 +22,52 @@ import static org.junit.Assert.assertEquals;
 @RunWith(MockitoJUnitRunner.class)
 public class RelayUnitTest {
 
+    @Mock
+    IRelay relay;
+
     @Test
     public void id_isCorrect() throws Exception {
-        IRelay relay = new Relay(1, "relay name", 2, false, false, false);
-        IModelValidator<IRelay> relayValidator = new RelayModelValidator();
-        String res = relayValidator.validate(relay);
-        assertEquals(null, res);
+        relay = new Relay(1, "relay name", 2, false, false, false);
+        RelayValidator<IRelay> relayValidator = new RelayModelValidator();
+        String res1 = relayValidator.validate(relay);
+        assertEquals(null, res1);
+        String res2 = ((RelayValidator)relayValidator).validateId(1);
+        assertEquals(res1, res2);
+        String res3 = ((RelayValidator)relayValidator).validateId(0);
+        assertNotEquals(res1, res3);
+        String res4 = ((RelayValidator)relayValidator).validateId(-1);
+        assertNotEquals(res1, res4);
+        assertEquals(res3, res4);
+    }
+
+    @Test
+    public void name_isCorrect() throws Exception {
+        relay = new Relay(1, "relay name", 2, false, false, false);
+        IModelValidator<IRelay> relayValidator = new RelayValidator();
+        String res1 = relayValidator.validate(relay);
+        assertEquals(null, res1);
+        String res2 = ((RelayValidator)relayValidator).validateName("relay name2");
+        assertEquals(res1, res2);
+        String res3 = ((RelayValidator)relayValidator).validateName("");
+        assertNotEquals(res1, res3);
+    }
+
+    @Test
+    public void gpio_isCorrect() throws Exception {
+        relay = new Relay(1, "relay name", 2, false, false, false);
+        IModelValidator<IRelay> relayValidator = new RelayValidator();
+        String res1 = relayValidator.validate(relay);
+        assertEquals(null, res1);
+        String res2 = ((RelayValidator)relayValidator).validateGpio(1);
+        assertEquals(res1, res2);
+        String res3 = ((RelayValidator)relayValidator).validateGpio(0);
+        assertNotEquals(res1, res3);
+        String res4 = ((RelayValidator)relayValidator).validateGpio(28);
+        assertNotEquals(res1, res4);
+        String res5 = ((RelayValidator)relayValidator).validateGpio("-1");
+        assertNotEquals(res1, res5);
+        String res6 = ((RelayValidator)relayValidator).validateGpio("1.2");
+        assertNotEquals(res1, res6);
+        assertEquals(res3, res6);
     }
 }
--- app/src/test/java/gcr/cli/android/ServerUnitTest.java
+++ app/src/test/java/gcr/cli/android/ServerUnitTest.java
@@ -5,14 +5,13 @@ import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 
-import gcr.cli.android.models.IRelay;
 import gcr.cli.android.models.IServerModel;
-import gcr.cli.android.models.Relay;
 import gcr.cli.android.models.realm.Server;
 import gcr.cli.android.validatiors.IModelValidator;
-import gcr.cli.android.validatiors.RelayModelValidator;
+import gcr.cli.android.validatiors.ServerValidator;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -24,13 +23,64 @@ public class ServerUnitTest {
 
     @Mock
     IServerModel server;
+    IModelValidator<IServerModel> validator;
+
+    ServerUnitTest() {
+        validator = new ServerValidator();
+    }
 
     @Test
     public void id_isCorrect() throws Exception {
-        server = new Server(1, );
-        IRelay relay = new Relay(1, "relay name", 2, false, false, false);
-        IModelValidator<IRelay> relayValidator = new RelayModelValidator();
-        String res = relayValidator.validate(relay);
-        assertEquals(null, res);
+        server = new Server(1, "server name", "10.0.0.3", 10001);
+        //validator = new ServerModelValidator();
+        String res1 = validator.validate(server);
+        assertEquals(null, res1);
+        String res2 = ((ServerValidator)validator).validateId(1);
+        assertEquals(res1, res2);
+        String res3 = ((ServerValidator)validator).validateId(0);
+        assertNotEquals(res1, res3);
+        String res4 = ((ServerValidator)validator).validateId(-1);
+        assertNotEquals(res1, res4);
+        assertEquals(res3, res4);
     }
+
+    @Test
+    public void name_isCorrect() throws Exception {
+        server = new Server(1, "server name", "10.0.0.3", 10001);
+        String res1 = validator.validate(server);
+        assertEquals(null, res1);
+        String res2 = ((ServerValidator)validator).validateName("server name2");
+        assertEquals(res1, res2);
+        String res3 = ((ServerValidator)validator).validateName("");
+        assertNotEquals(res1, res3);
+    }
+
+    @Test
+    public void address_isCorrect() throws Exception {
+        server = new Server(1, "server name", "10.0.0.3", 10001);
+        String res1 = validator.validate(server);
+        assertEquals(null, res1);
+        String res2 = ((ServerValidator)validator).validateAddress("relay name2");
+        assertEquals(res1, res2);
+        String res3 = ((ServerValidator)validator).validateAddress("");
+        assertNotEquals(res1, res3);
+    }
+
+//    @Test
+//    public void socketPort_isCorrect() throws Exception {
+//        server = new Server(1, "server name", "10.0.0.3", 10001);
+//        String res1 = relayValidator.validate(relay);
+//        assertEquals(null, res1);
+//        String res2 = ((RelayModelValidator)relayValidator).validateGpio(1);
+//        assertEquals(res1, res2);
+//        String res3 = ((RelayModelValidator)relayValidator).validateGpio(0);
+//        assertNotEquals(res1, res3);
+//        String res4 = ((RelayModelValidator)relayValidator).validateGpio(28);
+//        assertNotEquals(res1, res4);
+//        String res5 = ((RelayModelValidator)relayValidator).validateGpio("-1");
+//        assertNotEquals(res1, res5);
+//        String res6 = ((RelayModelValidator)relayValidator).validateGpio("1.2");
+//        assertNotEquals(res1, res6);
+//        assertEquals(res3, res6);
+//    }
 }
