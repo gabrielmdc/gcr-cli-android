f83b8b9c708bbe31ea2387acb77ab661239d2966
Near Lg <nearlg@gmail.com>
2018-03-24 17:34:24 +0100

    Refactor: refactor validators

--- app/src/main/java/gcr/cli/android/validatiors/RelayValidator.java
+++ app/src/main/java/gcr/cli/android/validatiors/RelayValidator.java
@@ -1,46 +1,65 @@
 package gcr.cli.android.validatiors;
 
-import gcr.cli.android.models.IRelay;
+import java.util.ArrayList;
+import java.util.List;
 
-public class RelayValidator extends ModelValidator<IRelay> {
+import gcr.cli.android.models.IRelay;
+import gcr.cli.android.validatiors.errorkeys.RelayErrorKeys;
+
+public class RelayValidator extends ModelValidator<IRelay, RelayErrorKeys> {
 
     public static final int ERROR_KEY_ID = 0;
 
-    public String validateId(int id) {
-        final String errorMsg = "Invalid id";
+    public RelayErrorKeys validateId(int id) {
         final String regex = "^[1-9]+[0-9]*$";
         final String idStr = id + "";
-        return validateData(regex, errorMsg, idStr);
+        return validateData(regex, RelayErrorKeys.ID, idStr);
     }
 
-    public String validateName(String name) {
-        final String errorMsg = "Invalid name";
+    public RelayErrorKeys validateName(String name) {
         final String regex = ".+";
-        return validateData(regex, errorMsg, name);
+        return validateData(regex, RelayErrorKeys.NAME, name);
     }
 
-    public String validateGpio(int gpio) {
+    public RelayErrorKeys validateGpio(int gpio) {
         final String gpioStr = gpio + "";
         return validateGpio(gpioStr);
     }
 
-    public String validateGpio(String gpioStr) {
-        final String errorMsg = "Invalid gpio";
+    public RelayErrorKeys validateGpio(String gpioStr) {
         final String regex = "^((2[0-7])|(1[0-9])|[1-9])$";
-        return validateData(regex, errorMsg, gpioStr);
+        return validateData(regex, RelayErrorKeys.GPIO, gpioStr);
     }
 
     @Override
-    public String validate(IRelay model) {
-        String msg = validateId(model.getId());
-        if(msg != null) {
-            return msg;
+    public List<RelayErrorKeys> validate(IRelay model) {
+        List<RelayErrorKeys> errorKeys = new ArrayList<>();
+
+        RelayErrorKeys errorKey = validateId(model.getId());
+        if(errorKey != null) {
+            errorKeys.add(errorKey);
         }
-        msg = validateName(model.getName());
-        if(msg != null) {
-            return msg;
+        errorKey = validateName(model.getName());
+        if(errorKey != null) {
+            errorKeys.add(errorKey);
         }
-        msg = validateGpio(model.getGpio());
-        return msg;
+        errorKey = validateGpio(model.getGpio());
+        if(errorKey != null) {
+            errorKeys.add(errorKey);
+        }
+        return errorKeys;
+    }
+
+    @Override
+    public String getErrorMessage(RelayErrorKeys key) {
+        switch(key) {
+            case ID:
+                return "Invalid id";
+            case NAME:
+                return "Invalid name";
+            case GPIO:
+                return "Invalid gpio";
+        }
+        return null;
     }
 }
--- app/src/main/java/gcr/cli/android/validatiors/ServerValidator.java
+++ app/src/main/java/gcr/cli/android/validatiors/ServerValidator.java
@@ -1,55 +1,75 @@
 package gcr.cli.android.validatiors;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import gcr.cli.android.models.IServerModel;
+import gcr.cli.android.validatiors.errorkeys.ServerErrorKeys;
 
-public class ServerValidator extends ModelValidator<IServerModel> {
+public class ServerValidator extends ModelValidator<IServerModel, ServerErrorKeys> {
 
-    public String validateId(int id) {
-        final String errorMsg = "Invalid id";
+    public ServerErrorKeys validateId(int id) {
         final String regex = "^[1-9][0-9]*$";
         final String idStr = id + "";
-        return validateData(regex, errorMsg, idStr);
+        return validateData(regex, ServerErrorKeys.ID, idStr);
     }
 
-    public String validateName(String name) {
-        final String errorMsg = "Invalid name";
+    public ServerErrorKeys validateName(String name) {
         final String regex = ".+";
-        return validateData(regex, errorMsg, name);
+        return validateData(regex, ServerErrorKeys.NAME, name);
     }
 
-    public String validateAddress(String address) {
-        final String errorMsg = "Invalid hostname address";
+    public ServerErrorKeys validateAddress(String address) {
         // Hostname specification: [RFC 1123] http://tools.ietf.org/html/rfc1123
         final String hostnameRegex = "^(([a-zA-Z0-9]|[a-zA-Z0-9][a-zA-Z0-9\\-]*[a-zA-Z0-9])\\.)*([A-Za-z0-9]|[A-Za-z0-9][A-Za-z0-9\\-]*[A-Za-z0-9])$";
         final String ipAddressRegex = "^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$";
-        if(validateData(hostnameRegex, errorMsg, address) != null) {
-            return validateData(ipAddressRegex, errorMsg, address);
+        if(validateData(hostnameRegex, ServerErrorKeys.ADDRESS, address) != null) {
+            return validateData(ipAddressRegex, ServerErrorKeys.ADDRESS, address);
         }
         return null;
     }
 
-    public String validateSocketPort(int socketPort) {
-        final String errorMsg = "Invalid socket port";
+    public ServerErrorKeys validateSocketPort(int socketPort) {
         final String regex = "^(6553[0-5]|655[0-2][0-9]|65[0-4][0-9]{2}|6[0-4][0-9]{3}|[1-5][0-9]{4}|[0-9]{1,4})$|^(6553[0-5]|655[0-2][0-9]|65[0-4][0-9]{2}|6[0-4][0-9]{3}|[1-5][0-9]{4}|[0-9]{1,4})$";
         final String socketPortStr = socketPort + "";
-        return validateData(regex, errorMsg, socketPortStr);
+        return validateData(regex, ServerErrorKeys.SOCKET_PORT, socketPortStr);
     }
 
     @Override
-    public String validate(IServerModel model) {
-        String msg = validateId(model.getId());
-        if(msg != null) {
-            return msg;
+    public List<ServerErrorKeys> validate(IServerModel model) {
+        List<ServerErrorKeys> errorKeys = new ArrayList<>();
+
+        ServerErrorKeys errorKey = validateId(model.getId());
+        if(errorKey != null) {
+            errorKeys.add(errorKey);
         }
-        msg = validateName(model.getName());
-        if(msg != null) {
-            return msg;
+        errorKey = validateName(model.getName());
+        if(errorKey != null) {
+            errorKeys.add(errorKey);
         }
-        msg = validateAddress(model.getAddress());
-        if(msg != null) {
-            return msg;
+        errorKey = validateAddress(model.getAddress());
+        if(errorKey != null) {
+            errorKeys.add(errorKey);
         }
-        msg = validateSocketPort(model.getSocketPort());
-        return msg;
+        errorKey = validateSocketPort(model.getSocketPort());
+        if(errorKey != null) {
+            errorKeys.add(errorKey);
+        }
+        return errorKeys;
+    }
+
+    @Override
+    public String getErrorMessage(ServerErrorKeys key) {
+        switch(key) {
+            case ID:
+                return "Invalid id";
+            case NAME:
+                return "Invalid name";
+            case ADDRESS:
+                return "Invalid hostname address";
+            case SOCKET_PORT:
+                return "Invalid socket port";
+        }
+        return null;
     }
 }
